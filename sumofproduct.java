5146471030c94a21810257e106b38b4afb423429
AMAR SARKAR <64923555+amarsarkar9@users.noreply.github.com>
2021-10-11 13:00:30 +0530

    updated one

--- sumofproduct.java
+++ sumofproduct.java
@@ -1,20 +1,23 @@
-package recursion;
-
-public class sumofproduct {
+public class SumOfProduct {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		System.out.println(sumOfProductOfDigits(35,6798));
+		System.out.println(sumOfProductOfDigits(32,3));
 
 	}
-	public static int sumOfProductOfDigits(int n1,int n2)
+	public static int sumOfProductOfDigits(int n1, int n2)
 	{
-	         if(n1==0 || n2==0)
-	        	 return 1;
-	         return (n1%10*n2%10)+sumOfProductOfDigits(n1/10,n2/10);
+	    // Your code here
+		int sum=0;
+	     if(n1>0 && n2>0)
+	     {
+		        	 return (n1 % 10) * (n2 % 10)+sumOfProductOfDigits(n1/10,n2/10);
+	     }
+	          
+		 return 0;
+	     
 	}
-}
-	  
+
+}  
 
 
